f2b4496589c1a005d5f634fbc66e50bcc9797d6b
Carleeene <hizenluna@gmail.com>
2025-02-08 00:52:54 +0800

    MSE and MSE Sample Variance
    Calculation for MSE and Sample Variance

---
+++ MSE Calculation.py
@@ -0,0 +1,27 @@
+#Mean Squared Error and Sample Variance calculation
+
+#per as Period
+per = int(input('Total Period of Operations: '))
+
+#Error Squared calculation
+#e as Error
+#eSquared as Error Squared
+def squared():
+    squared = 0
+    e = []
+    for i in range(per):
+        er = int(input(f'Enter the Error Value for Period {1+i}: '))
+        e.append(er)
+    
+    for x in e:
+        squared += abs(x**2)
+    return squared
+
+eSquared = squared()
+
+#Calculate the MSE and Sample Variance
+#mse_Sample as Mean Squared Error Sample Variance
+#mse as Mean Squared Error
+mse_Sample = eSquared/(per-1)
+mse = eSquared/per
+print(f'MSE is {mse}\nMSE Sample Variance is {mse_Sample}')
